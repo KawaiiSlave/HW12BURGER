e6608bf07eb5de2c1121b266893c9da9baadb351
Tyler <tgandle@gmail.com>
2020-02-05 13:54:24 -0600

    changed a few things around in the api-routes for further testing

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -18,7 +18,6 @@ module.exports = function(app) {
       burger_name: req.body.burger_name,
       devoured: req.body.devoured,
     }).then(function(dbburgers) {
-      // `results` here would be the newly created Burger
       res.json(dbburgers);
     });
 
@@ -27,11 +26,8 @@ module.exports = function(app) {
 };
 
 
-app.put("/api/Burgers", function(req, res) {
-  // Update takes in an object describing the properties we want to update, and
-  // we use where to describe which objects we want to update
+app.put("/api/Burgers/:id", function(req, res) {
   db.burgers.update({
-    burger_name: req.body.burger_name,
     devoured: req.body.devoured
   }, {
     where: {
