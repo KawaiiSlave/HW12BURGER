1635ddbe909fb49f9cda38152820b2b5eff5573c
Tyler <tgandle@gmail.com>
2020-02-05 13:32:35 -0600

    messed with, and added GET, POST, AND PUT routes in the api-routes file

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,32 +1,44 @@
-var Burger = require("../models/burgers.js");
+let db = require("../models");
 
 
 
 module.exports = function(app) {
 
   // Get all Burgers
-  app.get("/", function(req, res) {
-    Burger.findAll({}).then(function(results) {
-      res.json(results);
+  app.get("/api/Burgers", function(req, res) {
+    db.burgers.findAll({}).then(function(dbburgers) {
+      res.json(dbburgers);
     });
 
   });
 
   // Add a Burger
-  app.post("/api/Burger", function(req, res) {
-
-    console.log("Burger Data:");
-    console.log(req.body);
-
+  app.post("/api/Burgers", function(req, res) {
     Burger.create({
       burger_name: req.body.burger_name,
       devoured: req.body.devoured,
-    
-    }).then(function(results) {
+    }).then(function(dbburgers) {
       // `results` here would be the newly created Burger
-      res.end();
+      res.json(dbburgers);
     });
 
   });
 
 };
+
+
+app.put("/api/Burgers", function(req, res) {
+  // Update takes in an object describing the properties we want to update, and
+  // we use where to describe which objects we want to update
+  db.burgers.update({
+    burger_name: req.body.burger_name,
+    devoured: req.body.devoured
+  }, {
+    where: {
+      id: req.body.id
+    }
+  }).then(function(dbburgers) {
+    res.json(dbburgers);
+  });
+});
+
